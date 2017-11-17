b47cf035c1f9d39d7ae5d92f6d9e9fd5edcb1214
lee dorian <leedorian88@abv.bg>
2017-11-17 11:30:53 +0200

    added indices generation

--- mid_project/generate_x0.m
+++ mid_project/generate_x0.m
@@ -1,13 +1,17 @@
-function x0 = generate_x0(dimension, cardinality, min, max)
+function [x0, indices] = generate_x0(dimension, cardinality, min_bounds, max_bounds)
 %Generate a sparse vector of cardinality. 
 %Draw at random the  locations of the non-zero entries (using Matlab's function). T
 %Then, draw at random (using the function ) the value of each entry from a uniform distribution in the range and multiply each by a random sign
 
 indices = randperm(dimension, cardinality);
 
-x0 = zeroes( dimension );
+x0 = zeros( 1, dimension );
 
-x0 = x0 + indices;
+for index=1:size(indices,2)
+   sign = ( round(rand()) * 2 - 1 );
+   x0( indices(index) ) = randn_bounds(min_bounds,max_bounds) * sign;
+end
+
 
 end
 
