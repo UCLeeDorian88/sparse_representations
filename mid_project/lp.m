703f5f449b32171b070d029e96e820b1e4e6274f
lee dorian <leedorian88@abv.bg>
2017-11-19 22:36:51 +0200

    added lp

--- mid_project/lp.m
+++ mid_project/lp.m
@@ -13,22 +13,25 @@ options = optimoptions('linprog','Algorithm','dual-simplex',...
 
 % TODO: Use Matlab's linprog function to solve the BP problem
 % Write your code here... x = ????;
-estimated_supp = [0];
+estimated_supp = [];
 
-[n, m] = size(A); 
-f = [zeros(m, 1); ones(m, 1)];
+n  = size(A,1);
+m  = size(A,2);
+x  = zeros(m,1);
 
-Ai = [-eye(m), -eye(m); eye(m), -eye(m)];
-bi = zeros(2*m, 1);
-x = linprog(f, Ai, bi, [A, zeros(n, m)], b);
-x = x(1:m);
+f = ones( 2 * m, 1 );
+
+x0 = linprog(f, [A, -A], b, [A, -A], b , zeros(2*m,1), 100000 * ones(2*m,1), 50000, options);
 
 estimated_supp = [];
+
+for i=1:m
+   x(i) =  x0(i) - x0(i + m);
+end
 
 for i=1:m
     if ( x(i) ~= 0 )
         estimated_supp = [estimated_supp i];    
     end
 end
-
 end
