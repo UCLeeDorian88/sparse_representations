7946c7defe20dc0546e467061da01bc1d8a3525a
lee dorian <leedorian88@abv.bg>
2017-11-30 08:25:42 +0200

    added compute psnr

--- final_project/compute_psnr.m
+++ final_project/compute_psnr.m
@@ -8,20 +8,22 @@ function [ psnr_val ] = compute_psnr(y_original, y_estimated)
 % Output:
 %  psnr_val - The Peak Signal to Noise Ratio (PSNR) score
 
-y_original = y_original(:);
-y_estimated = y_estimated(:);
+y_original      = y_original(:);
+y_estimated     = y_estimated(:);
 
 % TODO: Compute the dynamic range
 % Write your code here... dynamic_range = ????;
-
+dynamic_range   = max(y_original) - min(y_original);
 
 % TODO: Compute the Mean Squared Error (MSE)
 % Write your code here... mse_val = ????;
 
+norm_val = norm( y_original - y_estimated);
+mse_val = (1.0/40.0) * norm_val * norm_val;
 
 % TODO: Compute the PSNR
 % Write your code here... psnr_val = ????;
-
+psnr_val = 10 * log10( (dynamic_range * dynamic_range) / (mse_val + 0.000001) );
 
 end
 
