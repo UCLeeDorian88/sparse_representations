de7ae7c5e10c7d461aa6a633565ab816bd08de6f
lee dorian <leedorian88@abv.bg>
2017-12-07 08:17:47 +0200

    changed omp

--- final_project/omp.m
+++ final_project/omp.m
@@ -1,70 +1,35 @@
-function x = omp(A, b, max_k)
+function x = omp(A, b, k)
 % OMP Solve the P0 problem via OMP
 %
 % Solves the following problem:
-%   min_x ||b - Ax||_2^2 s.t. ||x||_0 <= max_k
+%   min_x ||b - Ax||_2^2 s.t. ||x||_0 <= k
 %
 % The solution is returned in the vector x
 
 % Initialize the vector x
-n  = size(A,1);
-m  = size(A,2);
+x = zeros(size(A,2),1);
+r = b;
+S = [];
 
-x    = zeros(m,1);
-r    = b;
-supp = [];
-k    = 0;
-
-%iteration
-while ( k <= max_k )
-
-    k = k+1;
+for i=1:k
+    % Choosing next atom:
+    corr   = abs(A'*r);
+    [~,id] = max(corr);
+    S      = [S id];
     
-    %compute e(i0) and i0
-    e_i0 = 0;
-    i0 = 1;
-    for i = 1:m
-        ai = A(:,i);
-        v = abs( ai'*r);
-        if ( v > e_i0)
-            i0  = i;
-            e_i0 = v;
-        end
-    end
-   
-    %add i0 to the support
-    %sort the columns so we do not change the basis elements
-    %may be not needed
-    %supp = sort( [supp i0] );
-    supp = [supp i0];
+    % Coefficient valus via LS:
+    As = A(:,S);
+    xs = pinv(As)*b;
+    x(S) = xs;
     
-    %preallocation of the columns
-    As = [];
-
-    %form As
-    for i = 1:size( supp, 2 )
-        As(:,i) = A(:,supp(i));
-    end
+    % Residual update:
+    r = b-A*x;
     
-    %solve the least squares problem
-    %xk = lsqminnorm(As,b);
-    xk = pinv(As)*b;
-
-    x  = zeros(m,1);
-
-    %put the values back into the solution
-    for i = 1:size( supp, 2 )
-        x( supp(i) ) = xk(i); 
-    end
-    
-    %update the residual
-    r = b - A*x;
-    
-    %bail out if we are very close
-    n_r = norm(r);
-    if ( n_r < 0.00001)
+    if norm(r)<1e-8
         break;
     end
 end
 
+
 end
+
